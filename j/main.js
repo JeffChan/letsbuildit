e007af8fd5a0b77ebba67dae995e9f024884f5a5
Jeff Chan <jeffchan@mit.edu>
2013-11-23 00:58:58 -0500

    RequireJS THREE

--- j/main.js
+++ j/main.js
@@ -10,10 +10,16 @@ requirejs.config({
     'underscore': 'lib/underscore',
     'filesaver': 'lib/FileSaver',
     'bootstrap': 'lib/bootstrap.min',
-    'bootstrap-slider': 'lib/bootstrap-slider'
+    'bootstrap-slider': 'lib/bootstrap-slider',
+    'three': 'three/three.min',
+    'three.CSG': 'three/ThreeCSG',
+    'three.TrackballControls': 'three/TrackballControls',
   },
   shim: {
     'underscore': { exports: '_' },
+    'three': { exports: 'THREE' },
+    'three.CSG': { deps: ['three'], exports: 'ThreeBSP' },
+    'three.TrackballControls': { deps: ['three'] },
     'bootstrap': { deps: ['jquery'] },
     'bootstrap-slider': { deps: ['jquery', 'bootstrap'] }
   }
